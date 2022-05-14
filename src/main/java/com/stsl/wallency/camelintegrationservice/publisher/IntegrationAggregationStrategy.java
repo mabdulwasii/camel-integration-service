c2b6210d58282810294608af728a65690dbaeaff
Saint Deemene <st.deemene@gmail.com>
2022-05-14 13:45:10 +0100

    updated

--- src/main/java/com/stsl/wallency/camelintegrationservice/publisher/IntegrationAggregationStrategy.java
+++ src/main/java/com/stsl/wallency/camelintegrationservice/publisher/IntegrationAggregationStrategy.java
@@ -19,7 +19,6 @@ public class IntegrationAggregationStrategy implements AggregationStrategy {
         ArrayList<Object> list;
         if (oldExchange == null) {
             list = new ArrayList<>();
-
             list.add(newBody);
             newExchange.getIn().setBody(list);
             return newExchange;
